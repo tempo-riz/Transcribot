52867732b21969b0ab201750fc09087042bf8b00
Thibault Chatillon <thibaultchatillon@gmail.com>
2023-04-18 19:34:36 +0200

    working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 const fs = require('fs');
 const https = require('https');
+const { Deepgram } = require('@deepgram/sdk');
 
+require('dotenv').config();
 
 // Create a new client instance
 const bot = new Client({
@@ -14,12 +15,18 @@ const bot = new Client({
     GatewayIntentBits.GuildMembers,
   ]
 });
+
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 bot.once(Events.ClientReady, c => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+// Initializes the Deepgram SDK
+const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
+
+console.log("Deepgram initialized");
 
 //audio if has attachment(s) and text is empty
 bot.on('messageCreate', message => {
@@ -31,27 +38,42 @@ bot.on('messageCreate', message => {
 
   message.attachments.forEach(attachment => {
     //check that attatchment is audio (looking in content type)
-    // console.log(attachment);
     if (!attachment.contentType.includes("audio")) {
       return;
     }
 
-    // console.log(attachment.url);
-    //downwload the audio file
-    const filename = `./audio/${message.authorId}`;
-    const file = fs.createWriteStream(filename);
-    const request = https.get(attachment.url, function (response) {
-      response.pipe(file);
+    console.log(attachment.url);
+    console.log("audio file processing...");
+
+    deepgram.transcription.preRecorded(
+      { url: attachment.url },
+      { punctuate: true, model: 'enhanced', language: 'fr' },
+    ).then((transcription) => {
+      // console.dir(transcription, { depth: null });
+      console.log(transcription.results?.channels[0]?.alternatives[0]?.transcript);
+      message.reply(transcription.results?.channels[0]?.alternatives[0]?.transcript);
+    }).catch((err) => {
+      console.log(err);
     });
 
-    //TODO
-    //transcribe the audio file
-    //send the text to the channel
-    //(delete the audio file)
+
+
+    //downwload the audio file
+    // const filename = `./audio/${attachment.id}`;
+    // const file = fs.createWriteStream(filename);
+    // const request = https.get(attachment.url, function (response) {
+    //   response.pipe(file);
+    // });
+
+
+
+
 
   });
 
 });
 
+
+
 // Log in to Discord with your client's token
-bot.login(token);
+bot.login(process.env.DISCORD_TOKEN);
